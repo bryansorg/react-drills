07a3c6597c744a3a1b9512827c0c16ed1baa7fd8
manxyrisen <bmanxy@gmail.com>
2018-05-07 22:07:16 -0500

    react drills day1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: ""
+    };
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
   render() {
+    console.log(this.state);
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input onChange={e => this.handleChange(e)} />
+        {this.state.userInput}
       </div>
     );
   }
 }
-
 export default App;
